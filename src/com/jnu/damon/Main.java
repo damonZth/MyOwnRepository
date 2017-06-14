dc3866eb79e6e4d414be799e1bc2f1ec51ae02b6
damonZth <822730025@qq.com>
2017-06-14 14:00:05 +0800

    2017.06.14 第一次提交代码。

---
+++ src/com/jnu/damon/Main.java
@@ -0,0 +1,9 @@
+package com.jnu.damon;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("高级项目学习第一天，2017.06.14");
+    }
+}
