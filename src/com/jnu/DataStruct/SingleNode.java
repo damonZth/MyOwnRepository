9cb8b4dee5749171a9490f1b9e43342221ff068e
damonZth <822730025@qq.com>
2017-06-14 18:55:57 +0800

    2017.06.14 数据结构节点类

---
+++ src/com/jnu/DataStruct/SingleNode.java
@@ -0,0 +1,42 @@
+package com.jnu.DataStruct;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+
+/**
+ * Created by Damon on 2017/6/14.
+ * 单向节点
+ * 节点只有一个指向后继节点的指针
+ */
+public class SingleNode<T> {
+    private T data;//节点元素的数据
+    private SingleNode<T> next;//后即节点
+
+    public void setData(T data){
+        this.data = data;
+    }
+    public T getData(){
+        return this.data;
+    }
+
+    public void setNext(SingleNode<T> next){
+        this.next = next;
+    }
+    public SingleNode getNext(){
+        return this.next;
+    }
+
+    public SingleNode(){
+        //空的构造器
+    }
+    public SingleNode(T data){
+        //带节点数据的构造器
+        this.data = data;
+    }
+
+    public SingleNode(T data, SingleNode<T> next){
+        //带节点数据和后继节点的构造器
+        this.data = data;
+        this.next = next;
+    }
+
+}
