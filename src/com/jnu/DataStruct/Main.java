77f38eb5c1587cce7e122b9b77d9ca85436b9ba0
damonZth <822730025@qq.com>
2017-06-15 20:49:38 +0800

    2017.06.15

--- src/com/jnu/DataStruct/Main.java
+++ src/com/jnu/DataStruct/Main.java
@@ -9,8 +9,8 @@ public class Main {
 //        ArrayQueue<Integer> aq = new ArrayQueue<>(3);
 //        print(1,aq.isEmpty());
 //        print(2,aq.isFull());
-////        print(6,aq.peek());
-////        print(7,aq.element());
+//        print(6,aq.peek());
+//        print(7,aq.element());
 //
 //        print(0,aq.add(0));
 //        print(0,aq.add(1));
@@ -21,8 +21,8 @@ public class Main {
 //        print(4,aq.isEmpty());
 //        print(5,aq.size());
 //
-////        aq.add(3);
-////        aq.offer(4);
+//        aq.add(3);
+//        aq.offer(4);
 //
 //        print(6,aq.peek());
 //        print(9,aq.poll());
@@ -41,35 +41,45 @@ public class Main {
 //        aq.clearQueue();
 //        print(3,aq.isFull());
 //
-////        print(9,aq.remove());
-        /*
-        SeqArrayQueue<Integer> sq = new SeqArrayQueue<>(3);
-        print(1,sq.isEmpty());
-        print(2,sq.isFull());
-        print(5,sq.size());
-        print(3,sq.add(2));
-        print(5,sq.size());
-        print(4,sq.add(3));
-        print(5,sq.size());
-        print(6,sq.isFull());
-        print(7,sq.add(4));
-         */
-        DynSeqArrayQueue<Integer> dynSeqArrayQueue = new DynSeqArrayQueue<>(3);
-        print(1,dynSeqArrayQueue.isEmpty());
-        print(2,dynSeqArrayQueue.isFull());
-        print(3,dynSeqArrayQueue.size());
-        print(4,dynSeqArrayQueue.add(3));
-        print(5,dynSeqArrayQueue.size());
-        print(6,dynSeqArrayQueue.add(4));
-        print(7,dynSeqArrayQueue.size());
-        print(8,dynSeqArrayQueue.isFull());
-        print(9,dynSeqArrayQueue.add(6));
-        print(10,dynSeqArrayQueue.isFull());
-        print(11,dynSeqArrayQueue.size());
+//        print(9,aq.remove());
+
+//        SeqArrayQueue<Integer> sq = new SeqArrayQueue<>(3);
+//        print(1,sq.isEmpty());
+//        print(2,sq.isFull());
+//        print(5,sq.size());
+//        print(3,sq.add(2));
+//        print(5,sq.size());
+//        print(4,sq.add(3));
+//        print(5,sq.size());
+//        print(6,sq.isFull());
+//        print(7,sq.add(4));
 
 
 
+//        DynSeqArrayQueue<Integer> dynSeqArrayQueue = new DynSeqArrayQueue<>(3);
+//        print(1,dynSeqArrayQueue.isEmpty());
+//        print(2,dynSeqArrayQueue.isFull());
+//        print(3,dynSeqArrayQueue.size());
+//        print(4,dynSeqArrayQueue.add(3));
+//        print(5,dynSeqArrayQueue.size());
+//        print(6,dynSeqArrayQueue.add(4));
+//        print(7,dynSeqArrayQueue.size());
+//        print(8,dynSeqArrayQueue.isFull());
+//        print(9,dynSeqArrayQueue.add(6));
+//        print(10,dynSeqArrayQueue.isFull());
+//        print(11,dynSeqArrayQueue.size());
 
+//        LinkQueue<Integer> lq = new LinkQueue<>();
+//        print(0,lq.isFull());
+//        print(0,lq.isEmpty());
+//        print(1,lq.add(2));
+//        print(2,lq.add(4));
+//        print(3,lq.size());
+//        print(4,lq.poll());
+//        print(5,lq.size());
+//        print(6,lq.peek());
+//        print(7,lq.remove());
+//        print(8,lq.size());
 
 
 //        DynSeqArrayQueue<Integer> daq = new DynSeqArrayQueue<>(2);
@@ -84,6 +94,7 @@ public class Main {
 //
 //        print(7,daq.isFull());
 //        print(8,daq.size());
+        System.out.println("测试");
 
     }
     /**
